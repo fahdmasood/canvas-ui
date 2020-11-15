fe25efe56c1596aae7f258059442775de5a29325
fahdmasood <fahdmasood99@hotmail.com>
2020-11-15 07:11:42 -0500

    init: initializing repository

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,7 +6,7 @@ const Root = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Dishant Mishra</title>
+        <title></title>
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap'
